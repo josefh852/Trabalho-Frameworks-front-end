4770eb865681431d64ac674a600ece5e1e19de10
josefh852 <153336694+josefh852@users.noreply.github.com>
2024-11-26 11:41:16 -0400

    atualizando css (gabi)

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,15 @@ type ProdutoType = {
   imagem: string
 }
 
-// Tipo para usuários
-type UsuarioType = {
-  id: number,
-  name: string,
-  email: string,
-  created_at: string,
-  updated_at: string
-}
-
 function App() {
   const [produtos, setProdutos] = useState<ProdutoType[]>([])
-  const [usuarios, setUsuarios] = useState<UsuarioType[]>([])
 
   // useEffect para carregar produtos e usuários
   useEffect(() => {
     // Buscar os produtos
-    fetch("http://localhost:8000/produtos")
+    fetch("https://trabalho-frameworks.onrender.com/produtos")
       .then(resposta => resposta.json())
       .then(dados => setProdutos(dados))
-
-    // Buscar os usuários
-    fetch("https://one022a-marketplace-e90o.onrender.com/usuarios")
-      .then(resposta => resposta.json())
-      .then(dados => setUsuarios(dados))
   }, [])
 
   return (
@@ -40,8 +25,7 @@ function App() {
 
 
 <header className="site-header">
-
-
+/
         <nav className="navigation">
           <ul>
             <li><a href="#login">Login</a></li>
@@ -73,23 +57,6 @@ function App() {
             ))
           }
         </div>
-      </div>
-
-      {/* Listagem de Usuários */}
-      <div className="usuarios-container">
-        <h1 className='titulo-usuario'>Usuários</h1>
-        <div className="usuarios-list"> {/* Adicionando wrapper */}
-          {
-            usuarios.map(usuario => (
-              <div key={usuario.id} className="usuario-item">
-                <h1 className="usuario-nome">{usuario.name}</h1>
-                <p>Email: {usuario.email}</p>
-                <p>Criado em: {new Date(usuario.created_at).toLocaleDateString()}</p>
-                <p>Atualizado em: {new Date(usuario.updated_at).toLocaleDateString()}</p>
-              </div>
-            ))
-          }
-        </div> {/* Fechando a div aqui */}
       </div>
     </>
   )
