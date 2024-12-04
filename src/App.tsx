a24b7ca9059a4c1893a1f4c419fc24c4701bd91d
josefh852 <153336694+josefh852@users.noreply.github.com>
2024-12-04 11:29:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
   return (
     <>
       <header className="site-header">
-        <img src="./ok.png" width={150} height={70}></img>
+        <img src="/ok.png" width={150} height={70}></img>
         <nav className="navigation">
           <ul>
             <li><a href="#login">Login</a></li>
